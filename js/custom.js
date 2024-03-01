d0b86b33fab116c30322a7a54c66a1bb44fd9b10
ThanGrove <than@virginia.edu>
2024-02-29 19:06:24 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.csc_bs_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
