f72a6bbcc46a3edb7e9e767ba8253c8660a0e750
ThanGrove <than@virginia.edu>
2024-03-27 16:09:30 -0400

    styling CSC custom footer

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,12 @@
   Drupal.behaviors.csc_bs_sass = {
     attach: function(context, settings) {
 
-      // Custom code here
+      $(document).ready(() => {
+        // Move node edit links etc. up to top admin nav bar. "Tabs" Block must be in a footer or somewhere on the page.
+        if ($('#toolbar-bar').length > 0 && $('#node-admin-links').length > 0) {
+          $('#node-admin-links').appendTo('#toolbar-bar');
+        }
+      });
 
     }
   };
