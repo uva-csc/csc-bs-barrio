5355711660d870895413b769a24ae35994fce232
ThanGrove <than@virginia.edu>
2024-10-02 15:34:55 -0400

    removing template for audience field and JS for view on home page

--- js/custom.js
+++ js/custom.js
@@ -55,13 +55,6 @@
           });
         });
 
-        // Event View audience fix
-        $('.views-field.audience').each((ind, el) => {
-          if ($(el).text().includes("to registration")) {
-            console.log('aud field', el);
-            $(el).text("Open for registration");
-          }
-        });
       }); // End of Document Ready
     }
   };
